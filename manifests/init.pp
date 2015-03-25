d44bebb5b69a4abca9466a5b4408f8df6aaf5fa1
Mike Farrow <contact@mikefarrow.co.uk>
2015-03-25 01:00:15 +0000

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,37 @@
+class zsh_env {
+
+	class { 'ohmyzsh':}
+
+	define setup_zsh() {
+
+		if $name == 'root' {
+			if $osname == 'Darwin' {
+				$home = "var/${name}"
+			} else {
+				$home = "/${name}"
+			}
+		} else {
+			$home = "${ohmyzsh::params::home}/${name}"
+		}
+
+		ohmyzsh::install { "${name}": }
+
+		file { "custom_zsh_theme for ${name}":
+			ensure => present,
+			path    => "${home}/.oh-my-zsh/themes/custom.zsh-theme",
+			source  => 'puppet:///modules/zsh_env/custom.zsh-theme',
+			require => Exec["ohmyzsh::git clone ${name}"],
+		}
+
+		ohmyzsh::theme { "${name}":
+			theme   => 'custom',
+			require => File["custom_zsh_theme for ${name}"],
+		}
+
+	}
+
+	# $users_split = split($users, "\n")
+	$users_split = ['vagrant']
+	setup_zsh { $users_split: }
+
+}
