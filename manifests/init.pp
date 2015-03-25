aa0b65d988bd1fbf69498164523129197e866295
Mike Farrow <contact@mikefarrow.co.uk>
2015-03-25 12:14:02 +0000

    adding custom zshrc and plugins

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,6 +25,21 @@ class zsh_env {
 			set_sh => true,
 		}
 
+		ohmyzsh::plugins { "${name}": plugins => 'git git-extras common-aliases' }
+
+		file_line { "${name}-custom-functions":
+			path    => "${home}/.zshrc",
+			line    => 'source $HOME/.zshrc_custom',
+			require => Ohmyzsh::Install[$name],
+		}
+
+		file { "custom .zshrc for ${name}":
+			ensure => present,
+			path    => "${home}/.zshrc_custom",
+			source  => 'puppet:///modules/zsh_env/zshrc_custom',
+			require => Exec["ohmyzsh::git clone ${name}"],
+		}
+
 		file { "custom_zsh_theme for ${name}":
 			ensure => present,
 			path    => "${home}/.oh-my-zsh/themes/custom.zsh-theme",
