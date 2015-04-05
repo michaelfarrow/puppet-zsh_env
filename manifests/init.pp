1545b8dce12910feeed0f28a2b1964124f9a1ebb
Mike Farrow <contact@mikefarrow.co.uk>
2015-04-05 05:30:16 +0100

    removing zsh install on Darwin for root

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,8 +5,10 @@ class zsh_env {
 		if $name == 'root' {
 			if $osname == 'Darwin' {
 				$home = "var/${name}"
+				$install = false
 			} else {
 				$home = "/${name}"
+				$install = true
 			}
 		} else {
 			if $osname == 'Darwin' {
@@ -14,42 +16,47 @@ class zsh_env {
 			} else {
 				$home = "/home/${name}"
 			}
+			$install = true
 		}
 
-		Exec <| title == "ohmyzsh::git clone ${name}" |> {
-			command => "git clone https://github.com/robbyrussell/oh-my-zsh.git ${home}/.oh-my-zsh",
-			cwd => $home,
-		}
+		if $install {
 
-		ohmyzsh::install { "${name}":
-			set_sh => true,
-		}
+			Exec <| title == "ohmyzsh::git clone ${name}" |> {
+				command => "git clone https://github.com/robbyrussell/oh-my-zsh.git ${home}/.oh-my-zsh",
+				cwd => $home,
+			}
 
-		ohmyzsh::plugins { "${name}": plugins => 'git git-extras common-aliases' }
+			ohmyzsh::install { "${name}":
+				set_sh => true,
+			}
 
-		file_line { "${name}-custom-functions":
-			path    => "${home}/.zshrc",
-			line    => 'source $HOME/.zshrc_custom',
-			require => Ohmyzsh::Install[$name],
-		}
+			ohmyzsh::plugins { "${name}": plugins => 'git git-extras common-aliases' }
 
-		file { "custom .zshrc for ${name}":
-			ensure => present,
-			path    => "${home}/.zshrc_custom",
-			source  => 'puppet:///modules/zsh_env/zshrc_custom',
-			require => Exec["ohmyzsh::git clone ${name}"],
-		}
+			file_line { "${name}-custom-functions":
+				path    => "${home}/.zshrc",
+				line    => 'source $HOME/.zshrc_custom',
+				require => Ohmyzsh::Install[$name],
+			}
 
-		file { "custom_zsh_theme for ${name}":
-			ensure => present,
-			path    => "${home}/.oh-my-zsh/themes/custom.zsh-theme",
-			source  => 'puppet:///modules/zsh_env/custom.zsh-theme',
-			require => Exec["ohmyzsh::git clone ${name}"],
-		}
+			file { "custom .zshrc for ${name}":
+				ensure => present,
+				path    => "${home}/.zshrc_custom",
+				source  => 'puppet:///modules/zsh_env/zshrc_custom',
+				require => Exec["ohmyzsh::git clone ${name}"],
+			}
 
-		ohmyzsh::theme { "${name}":
-			theme   => 'custom',
-			require => File["custom_zsh_theme for ${name}"],
+			file { "custom_zsh_theme for ${name}":
+				ensure => present,
+				path    => "${home}/.oh-my-zsh/themes/custom.zsh-theme",
+				source  => 'puppet:///modules/zsh_env/custom.zsh-theme',
+				require => Exec["ohmyzsh::git clone ${name}"],
+			}
+
+			ohmyzsh::theme { "${name}":
+				theme   => 'custom',
+				require => File["custom_zsh_theme for ${name}"],
+			}
+
 		}
 
 	}
